b03dde902aa7aa933ba462cd0abcc7cec4bc2b7c
Zachary Diehl <zachary.diehl@gmail.com>
2017-09-26 16:26:14 -0400

    minor style tweaks to team page and re-wrote bio

--- src/components/team-container/index.js
+++ src/components/team-container/index.js
@@ -8,7 +8,7 @@ class TeamContainer extends React.Component {
       <div className='team'>
         <div>
           <img src='https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/20993067_10159219337715274_2489403136085593342_n.jpg?oh=a4b6b3d02f4f4454d7d3f84bf2b4d9db&oe=5A5C65F1' />
-          <p>The Warchief (Physical Resilience Coach) - 10 year combat veteran leading teams in austere environments, he has retired his uniform and automatic weapons for weighted clothing and warhammers.  The warchief has turned his lust for battle, into an obsession with training individuals and teams.  Through physical resilience, his students will learn their bodies and minds are capable of far greater things they can imagine.  His training will teach you to embrace pain and fear as companions in the journey to becoming a legend. </p>
+          <p><h5>The Warchief</h5>After spending 10 years fighting battles in foreign lands, the warchief has hung up his uniform to embark on new adventures.  He is a warrior at heart who believes that peace is possible and battle should be a last resort.  He is commited to training a new generation of warriors who understand the incredible strength in determination and resilience.  He is adamant in teaching his students that pride is built on sand, true power comes from within.</p>
         </div>
       </div>
     )
